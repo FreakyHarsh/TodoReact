6d094e7353865103062a6caf93db5e4ddd038bb4
FreakyHarsh <harsh.boricha2015@gmail.com>
2020-11-27 20:32:28 +0530

    themes checking

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isLightTheme, setisLightTheme] = useState(true);
+  const [theme, setTheme] = useState('light');
+
+  const toggler = () => {
+    setisLightTheme(!isLightTheme);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={isLightTheme ? 'light bg' : 'dark bg'}>
+        <p>Toggler theme is here</p>
+      </div>
+      <button onClick={toggler}>{isLightTheme ? 'Dark' : 'Light'}</button>
+      <p className={theme + ' bg'}>Theme toggler for selected radio buttons</p>
+      <input type="radio" name="theme" value="blue" id="blue" onClick={(e) => setTheme(e.target.value)} />
+      <label htmlFor="blue">Blue</label>
+      <input type="radio" name="theme" value="dark" id="dark" onClick={(e) => setTheme(e.target.value)} />
+      <label htmlFor="dark">Dark</label>
+      <input type="radio" name="theme" value="white" id="white" onClick={(e) => setTheme(e.target.value)} />
+      <label htmlFor="white">White</label>
+      <br />
     </div>
   );
 }
